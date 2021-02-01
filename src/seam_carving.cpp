47e942113842595365785ffa552fa9d8f45cdb5d
MeHow77 <michal.hawa@gmail.com>
2021-02-01 16:31:07 +0100

    energy function based on Sobel

--- src/seam_carving.cpp
+++ src/seam_carving.cpp
@@ -1,4 +1,24 @@
 #include "seam_carving.h"
+cv::Mat calc_e1(const cv::Mat &image){
+    int ddepth = CV_16S;
+    int ksize = 5;
+
+    cv::Mat srcBlur, srcGray, e1;
+    cv::GaussianBlur(image, srcBlur, cv::Size(3,3), 0, 0);
+    cv::cvtColor(srcBlur, srcGray, cv::COLOR_BGR2GRAY);
+
+    cv::Mat gradX, gradY;
+    cv::Mat absGradX, absGradY;
+
+    cv::Sobel(srcGray, gradX, ddepth, 1, 0, ksize);
+    cv::Sobel(srcGray, gradY, ddepth, 0, 1, ksize);
+
+    cv::convertScaleAbs(gradX, absGradX);
+    cv::convertScaleAbs(gradY, absGradY);
+
+    cv::addWeighted(absGradX, 0.5, absGradY, 0.5, 0, e1);
+    return e1;
+}
 
 cv::Mat seamCarving(const cv::Mat& image, const cv::Size& out_size) {
   (void)out_size;
--- src/seam_carving.h
+++ src/seam_carving.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/mat.hpp>
 
+cv::Mat calc_e1(const cv::Mat &image);
 cv::Mat seamCarving(const cv::Mat& image, const cv::Size& out_size);
